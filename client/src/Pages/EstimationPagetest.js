74c89f4a9aa5b0fd64405f5a22180584e6058cb2
Monicaprojects21 <112512124+Monicaprojects21@users.noreply.github.com>
2023-05-04 12:47:51 -0400

    committest11

--- client/src/Pages/EstimationPagetest.js
+++ client/src/Pages/EstimationPagetest.js
@@ -48,3 +48,39 @@ test("clicking continue button navigates to customer onboarding page", () => {
   fireEvent.click(continueButton);
   expect(window.location.pathname).toBe("/CustomerOnBoardingPage");
 });
+
+test("checkbox label contains link to terms and conditions", () => {
+  const { getByText } = render(<EstimationPage />);
+  const checkboxLabel = getByText(/I agree to the/);
+  expect(checkboxLabel).toContainElement(getByText(/terms and conditions/i));
+});
+
+test("continue button is disabled when checkbox is unchecked", () => {
+  const { getByLabelText, getByText } = render(<EstimationPage />);
+  const checkbox = getByLabelText(/I agree to the terms and conditions/i);
+  const continueButton = getByText(/continue/i);
+  expect(continueButton).toBeDisabled();
+  fireEvent.click(checkbox);
+  expect(continueButton).toBeEnabled();
+});
+
+test("customer's agreed state is passed to customer onboarding page", () => {
+  const { getByLabelText, getByText } = render(
+    <MemoryRouter initialEntries={["/estimation"]}>
+      <Route exact path="/estimation" component={EstimationPage} />
+      <Route
+        exact
+        path="/CustomerOnBoardingPage"
+        component={CustomerOnBoardingPage}
+      />
+    </MemoryRouter>
+  );
+  const checkbox = getByLabelText(/I agree to the terms and conditions/i);
+  fireEvent.click(checkbox);
+  const continueButton = getByText(/continue/i);
+  fireEvent.click(continueButton);
+  const onboardingPage = getByText(/customer onboarding page/i);
+  expect(onboardingPage).toBeInTheDocument();
+  const agreedState = getByText(/agreed: true/i);
+  expect(agreedState).toBeInTheDocument();
+});
