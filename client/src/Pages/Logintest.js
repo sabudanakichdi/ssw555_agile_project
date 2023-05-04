e67f469474d4505c05e64844bfaebd765c4a461c
Monicaprojects21 <112512124+Monicaprojects21@users.noreply.github.com>
2023-05-04 12:54:04 -0400

    committest12

--- client/src/Pages/Logintest.js
+++ client/src/Pages/Logintest.js
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import Login from "./Login";
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 test("renders Login component without errors", () => {
   render(<Login />);
@@ -62,4 +63,59 @@ test("renders link to Signup page correctly", () => {
   const signUpLink = screen.getByRole("link", { name: /sign up here/i });
   expect(signUpLink).toBeInTheDocument();
   expect(signUpLink).toHaveAttribute("href", "/Signup");
+});
+
+test("email input field is empty by default", () => {
+  render(<Login />);
+  const emailInput = screen.getByLabelText(/email address/i);
+  expect(emailInput.value).toBe("");
+});
+
+test("password input field is empty by default", () => {
+  render(<Login />);
+  const passwordInput = screen.getByLabelText(/password/i);
+  expect(passwordInput.value).toBe("");
+});
+
+test("email input field updates correctly", () => {
+  render(<Login />);
+  const emailInput = screen.getByLabelText(/email address/i);
+  const testEmail = "test@example.com";
+  userEvent.type(emailInput, testEmail);
+  expect(emailInput.value).toBe(testEmail);
+});
+
+test("password input field updates correctly", () => {
+  render(<Login />);
+  const passwordInput = screen.getByLabelText(/password/i);
+  const testPassword = "password123";
+  userEvent.type(passwordInput, testPassword);
+  expect(passwordInput.value).toBe(testPassword);
+});
+
+test("sign in button is disabled when email and/or password fields are empty", () => {
+  render(<Login />);
+  const emailInput = screen.getByLabelText(/email address/i);
+  const passwordInput = screen.getByLabelText(/password/i);
+  const signInButton = screen.getByRole("button", { name: /sign in/i });
+  expect(signInButton).toBeDisabled();
+  userEvent.type(emailInput, "test@example.com");
+  expect(signInButton).toBeDisabled();
+  userEvent.type(passwordInput, "password123");
+  expect(signInButton).toBeEnabled();
+});
+
+test("clicking sign in button triggers login request", async () => {
+  const mockLogin = jest.fn();
+  render(<Login onLogin={mockLogin} />);
+  const emailInput = screen.getByLabelText(/email address/i);
+  const passwordInput = screen.getByLabelText(/password/i);
+  const signInButton = screen.getByRole("button", { name: /sign in/i });
+  userEvent.type(emailInput, "test@example.com");
+  userEvent.type(passwordInput, "password123");
+  userEvent.click(signInButton);
+  expect(mockLogin).toHaveBeenCalledWith({
+    email: "test@example.com",
+    password: "password123",
+  });
 });
