bc74c892d4b2b0e1e9309bb42ebc06d89f43ffef
Vishnu1604-github <123218811+Vishnu1604-github@users.noreply.github.com>
2023-05-04 16:29:40 -0400

    committest2

--- client/src/Pages/ContactUstest.js
+++ client/src/Pages/ContactUstest.js
@@ -1,121 +1,50 @@
-import { render } from "@testing-library/react";
-import ContactUs from "./ContactUs";
-import { fireEvent } from "@testing-library/react";
-test("renders ContactUs component without crashing", () => {
-  render(<ContactUs />);
-});
-test('renders "Contact Sales" header', () => {
-  const { getByText } = render(<ContactUs />);
-  const headerElement = getByText(/Contact Sales/i);
-  expect(headerElement).toBeInTheDocument();
-});
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import ContactUs from './ContactUs';
 
-test('renders "Let\'s Talk" button', () => {
-  const { getByRole } = render(<ContactUs />);
-  const buttonElement = getByRole("button", { name: /Let's Talk/i });
-  expect(buttonElement).toBeInTheDocument();
-});
+describe('ContactUs', () => {
+  it('renders the form correctly', () => {
+    render(<ContactUs />);
 
-test("renders all required input fields", () => {
-  const { getByLabelText } = render(<ContactUs />);
-  const firstNameElement = getByLabelText(/First Name/i);
-  const lastNameElement = getByLabelText(/Last Name/i);
-  const companyElement = getByLabelText(/Company/i);
-  const emailElement = getByLabelText(/Email/i);
-  const phoneElement = getByLabelText(/Phone Number/i);
-  const messageElement = getByLabelText(/Message/i);
+    expect(screen.getByLabelText('First Name')).toBeInTheDocument();
+    expect(screen.getByLabelText('Last Name')).toBeInTheDocument();
+    expect(screen.getByLabelText('Company')).toBeInTheDocument();
+    expect(screen.getByLabelText('Email')).toBeInTheDocument();
+    expect(screen.getByLabelText('Phone Number')).toBeInTheDocument();
+    expect(screen.getByLabelText('Message')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Let\'s Talk' })).toBeInTheDocument();
+  });
 
-  expect(firstNameElement).toBeInTheDocument();
-  expect(lastNameElement).toBeInTheDocument();
-  expect(companyElement).toBeInTheDocument();
-  expect(emailElement).toBeInTheDocument();
-  expect(phoneElement).toBeInTheDocument();
-  expect(messageElement).toBeInTheDocument();
-});
+  it('submits the form with valid data', () => {
+    const mockSubmit = jest.fn();
+    render(<ContactUs onSubmit={mockSubmit} />);
 
-test('disables "Let\'s Talk" button when required fields are empty', () => {
-  const { getByRole } = render(<ContactUs />);
-  const buttonElement = getByRole("button", { name: /Let's Talk/i });
+    fireEvent.change(screen.getByLabelText('First Name'), { target: { value: 'John' } });
+    fireEvent.change(screen.getByLabelText('Last Name'), { target: { value: 'Doe' } });
+    fireEvent.change(screen.getByLabelText('Company'), { target: { value: 'ACME Inc.' } });
+    fireEvent.change(screen.getByLabelText('Email'), { target: { value: 'john.doe@example.com' } });
+    fireEvent.change(screen.getByLabelText('Phone Number'), { target: { value: '1234567890' } });
+    fireEvent.change(screen.getByLabelText('Message'), { target: { value: 'Hello, I\'m interested in your product' } });
+    fireEvent.click(screen.getByRole('button', { name: 'Let\'s Talk' }));
 
-  expect(buttonElement).toBeDisabled();
+    expect(mockSubmit).toHaveBeenCalledTimes(1);
+    expect(mockSubmit).toHaveBeenCalledWith({
+      firstName: 'John',
+      lastName: 'Doe',
+      company: 'ACME Inc.',
+      email: 'john.doe@example.com',
+      phoneNumber: '1234567890',
+      message: 'Hello, I\'m interested in your product',
+    });
+  });
 
-  const firstNameElement = getByRole("textbox", { name: /First Name/i });
-  fireEvent.change(firstNameElement, { target: { value: "John" } });
+  it('shows an error message for missing required fields', () => {
+    const mockSubmit = jest.fn();
+    render(<ContactUs onSubmit={mockSubmit} />);
 
-  expect(buttonElement).toBeDisabled();
+    fireEvent.click(screen.getByRole('button', { name: 'Let\'s Talk' }));
 
-  const lastNameElement = getByRole("textbox", { name: /Last Name/i });
-  fireEvent.change(lastNameElement, { target: { value: "Doe" } });
-
-  expect(buttonElement).toBeDisabled();
-
-  const companyElement = getByRole("textbox", { name: /Company/i });
-  fireEvent.change(companyElement, { target: { value: "Acme Inc." } });
-
-  expect(buttonElement).toBeDisabled();
-
-  const emailElement = getByRole("textbox", { name: /Email/i });
-  fireEvent.change(emailElement, { target: { value: "john.doe@acme.com" } });
-
-  expect(buttonElement).toBeDisabled();
-
-  const phoneElement = getByRole("textbox", { name: /Phone Number/i });
-  fireEvent.change(phoneElement, { target: { value: "1234567890" } });
-
-  expect(buttonElement).toBeDisabled();
-
-  const messageElement = getByRole("textbox", { name: /Message/i });
-  fireEvent.change(messageElement, { target: { value: "Hello world!" } });
-});
-
-test("submitting the form calls the onSubmit function", () => {
-  const handleSubmit = jest.fn();
-  const { getByRole } = render(<ContactUs onSubmit={handleSubmit} />);
-  const buttonElement = getByRole("button", { name: /Let's Talk/i });
-  fireEvent.click(buttonElement);
-  expect(handleSubmit).toHaveBeenCalled();
-});
-
-test("submitting the form clears input fields", () => {
-  const handleSubmit = jest.fn();
-  const { getByRole, getByLabelText } = render(
-    <ContactUs onSubmit={handleSubmit} />
-  );
-  const firstNameElement = getByLabelText(/First Name/i);
-  const lastNameElement = getByLabelText(/Last Name/i);
-  const companyElement = getByLabelText(/Company/i);
-  const emailElement = getByLabelText(/Email/i);
-  const phoneElement = getByLabelText(/Phone Number/i);
-  const messageElement = getByLabelText(/Message/i);
-  const buttonElement = getByRole("button", { name: /Let's Talk/i });
-
-  fireEvent.change(firstNameElement, { target: { value: "John" } });
-  fireEvent.change(lastNameElement, { target: { value: "Doe" } });
-  fireEvent.change(companyElement, { target: { value: "Acme Inc." } });
-  fireEvent.change(emailElement, { target: { value: "john.doe@acme.com" } });
-  fireEvent.change(phoneElement, { target: { value: "1234567890" } });
-  fireEvent.change(messageElement, { target: { value: "Hello world!" } });
-  fireEvent.click(buttonElement);
-
-  expect(firstNameElement.value).toBe("");
-  expect(lastNameElement.value).toBe("");
-  expect(companyElement.value).toBe("");
-  expect(emailElement.value).toBe("");
-  expect(phoneElement.value).toBe("");
-  expect(messageElement.value).toBe("");
-});
-
-test("submitting the form with invalid email displays error message", () => {
-  const handleSubmit = jest.fn();
-  const { getByRole, getByLabelText, getByText } = render(
-    <ContactUs onSubmit={handleSubmit} />
-  );
-  const emailElement = getByLabelText(/Email/i);
-  const buttonElement = getByRole("button", { name: /Let's Talk/i });
-
-  fireEvent.change(emailElement, { target: { value: "invalidemail" } });
-  fireEvent.click(buttonElement);
-
-  expect(handleSubmit).not.toHaveBeenCalled();
-  expect(getByText(/Please enter a valid email address/i)).toBeInTheDocument();
+    expect(mockSubmit).not.toHaveBeenCalled();
+    expect(screen.getByText('Please fill out this field.')).toBeInTheDocument();
+  });
 });
