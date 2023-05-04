9ca1a1aa3decfc221bda212f633dcade6cdb3177
Vishnu1604-github <123218811+Vishnu1604-github@users.noreply.github.com>
2023-05-04 17:05:42 -0400

    committest10

--- client/src/Pages/Trackingtest.js
+++ client/src/Pages/Trackingtest.js
@@ -24,3 +24,32 @@ describe("Tracking component", () => {
     expect(inputElement.value).toBe("2023-05-10");
   });
 });
+test('renders Order Summary heading', () => {
+  render(<Tracking />);
+  const orderSummaryHeading = screen.getByText(/Order Summary:/i);
+  expect(orderSummaryHeading).toBeInTheDocument();
+});
+test('renders Desired delivery date label', () => {
+  render(<Tracking />);
+  const deliveryDateLabel = screen.getByLabelText(/Desired delivery date:/i);
+  expect(deliveryDateLabel).toBeInTheDocument();
+});
+test('updates deliveryDate state when input value changes', () => {
+  render(<Tracking />);
+  const input = screen.getByLabelText(/Desired delivery date:/i);
+  fireEvent.change(input, { target: { value: '2023-05-10' } });
+  expect(input).toHaveValue('2023-05-10');
+});
+test('calls handleDeliveryDateChange function when input value changes', () => {
+  const handleDeliveryDateChange = jest.fn();
+  render(<Tracking handleDeliveryDateChange={handleDeliveryDateChange} />);
+  const input = screen.getByLabelText(/Desired delivery date:/i);
+  fireEvent.change(input, { target: { value: '2023-05-10' } });
+  expect(handleDeliveryDateChange).toHaveBeenCalledTimes(1);
+});
+test('sets input value prop to deliveryDate state', () => {
+  render(<Tracking />);
+  const input = screen.getByLabelText(/Desired delivery date:/i);
+  fireEvent.change(input, { target: { value: '2023-05-10' } });
+  expect(input).toHaveValue('2023-05-10');
+});
