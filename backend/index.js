02ad0d27bee75e2b1ba52f40b68f55d7ed92e6a4
sabudanakichdi <pchauha2@stevens.edu>
2023-05-04 20:10:58 -0400

    Added CORS

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./src/config/db');
 const path = require('path');
+const cors = require('cors');
 //connect to Database
 connectDB()
 
@@ -10,6 +11,7 @@ connectDB()
 
 // initialise middleware
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 // Define Routes
 app.use('/api/register', require('./src/routes/users'))
@@ -23,6 +25,21 @@ app.use('/api/tracking', require('./src/routes/salesDetail'))
 app.use('/api/isToggle', require('./src/routes/agreementToggle'))
 //app.use('/api/auth', require('./src/routes/auth'))
 
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+app.use(cors({
+    methods: ['GET', 'POST', 'PUT']
+}));
+
+app.use(cors({
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
+}));
+
+app.use(cors({
+    maxAge: 86400 // One day
+}));
 
 
 ////server static assets in production
@@ -39,7 +56,7 @@ app.use('/api/isToggle', require('./src/routes/agreementToggle'))
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Start the server @ port 
 app.listen(PORT, () => {
